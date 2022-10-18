69eea89623221f96c59f41f2c9726cb0ea31178c
Sxmtee <somtoo76@gmail.com>
2022-10-18 23:55:20 +0100

    foodbody

--- lib/HomeScreens/food_body.dart
+++ lib/HomeScreens/food_body.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_indicator/carousel_indicator.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,20 +23,37 @@ class _FoodPageBodyState extends State<FoodPageBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.red,
-        height: 300,
-        child: CarouselSlider.builder(
-            carouselController: controller,
-            itemCount: 5,
-            itemBuilder: ((context, index, realIndex) {
-              return _buildPageItem(index);
-            }),
-            options: CarouselOptions(
-                viewportFraction: 0.85,
-                height: 300,
-                autoPlay: true,
-                enlargeCenterPage: true)));
+    return Column(
+      children: [
+        Container(
+            //color: Colors.red,
+            height: 300,
+            child: CarouselSlider.builder(
+                carouselController: controller,
+                itemCount: 5,
+                itemBuilder: ((context, index, realIndex) {
+                  return _buildPageItem(index);
+                }),
+                options: CarouselOptions(
+                  viewportFraction: 0.85,
+                  height: 300,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      currentPage = index;
+                    });
+                  },
+                ))),
+        CarouselIndicator(
+          width: 10,
+          count: 5,
+          index: currentPage,
+          color: AppColors.mainBlackColor,
+          activeColor: AppColors.mainColor,
+        ),
+      ],
+    );
   }
 
   Widget _buildPageItem(int index) {
@@ -56,9 +74,23 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             height: 120,
             margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: AppColors.allWhite,
-            ),
+                borderRadius: BorderRadius.circular(30),
+                color: AppColors.allWhite,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.allWhite,
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5),
+                  ),
+                  BoxShadow(
+                    color: AppColors.allWhite,
+                    offset: Offset(-5, 0),
+                  ),
+                  BoxShadow(
+                    color: AppColors.allWhite,
+                    offset: Offset(5, 0),
+                  ),
+                ]),
             child: Container(
               padding: EdgeInsets.only(top: 15, left: 15, right: 15),
               child: Column(
